08bc0f8f07e05b6a4e4a77dcdf50f646d007aafc
Jeneliya12 <gurungjeneliya@gmail.com>
2024-08-14 23:01:02 -0400

    usecontext applied

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,10 +1,12 @@
+import React from "react";
 import Mainsection from "../components/mainsection/mainsection";
+import { WeatherProvider } from "../components/mainsection/weathercontext";
 
 function Home() {
   return (
-    <>
+    <WeatherProvider>
       <Mainsection />
-    </>
+    </WeatherProvider>
   );
 }
 export default Home;
